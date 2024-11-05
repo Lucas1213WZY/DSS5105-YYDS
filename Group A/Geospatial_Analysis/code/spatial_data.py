a4890c48bf568a867929d0f9207309b5d9babc7b
WongHuiHern <e1352836@u.nus.edu>
2024-11-05 17:27:53 +0800

    Add files via upload

--- Group A/Geospatial_Analysis/code/spatial_data.py
+++ Group A/Geospatial_Analysis/code/spatial_data.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from sklearn.cluster import DBSCAN
 from scipy.spatial import ConvexHull
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point, Polygon, MultiPolygon
 import geopandas as gpd
 import numpy as np
 
-class RegionClassifierWithConvexHull:
+class hh_RegionClassifierWithConvexHull:
     def __init__(self, poi_file, eps=0.01, min_samples=3):
         self.poi_data = pd.read_excel(poi_file)
         self.poi_data = self.poi_data.dropna(subset=['region_name'])
@@ -80,6 +80,51 @@ class RegionClassifierWithConvexHull:
         plt.show()
 
         return new_data
+
+class RegionClassifierWithConvexHull: # Use this for user
+    def __init__(self, geojson_file):
+        self.region_data = gpd.read_file(geojson_file)
+        self.region_hulls = {}
+
+        for _, row in self.region_data.iterrows():
+            region_name = row['region_name']
+            region_geometry = row['geometry']
+
+            if isinstance(region_geometry, Polygon):
+                region_points = list(region_geometry.exterior.coords)
+            elif isinstance(region_geometry, MultiPolygon):
+                region_points = list(region_geometry.geoms[0].exterior.coords)
+            else:
+                continue
+
+            region_points = [(point[0], point[1]) for point in region_points]
+            if len(region_points) >= 3:
+                hull = ConvexHull(region_points)
+                hull_points = [region_points[vertex] for vertex in hull.vertices]
+                self.region_hulls[region_name] = Polygon(hull_points)
+            else:
+                self.region_hulls[region_name] = None
+
+    def classify(self, new_point): # Use for classify one point
+        point = Point(new_point['latitude'], new_point['longitude'])
+        region_name = "Unknown"
+        for region, polygon in self.region_hulls.items():
+            if polygon and polygon.contains(point):
+                region_name = region
+                break
+        return region_name
+
+    def classify_new_points(self, new_data_file): # Use for classifying points in a file
+        new_data = pd.read_excel(new_data_file)
+        classified_regions = []
+
+        for _, row in new_data.iterrows():
+            region_name = self.classify({'latitude': row['latitude'], 'longitude': row['longitude']})
+            classified_regions.append(region_name)
+
+        new_data['region_name'] = classified_regions
+        return new_data
+
 
 class InsideBuilding:
     def __init__(self, total_emission, a=0.6, b=0.25, c=0.15, x=1, y=0.4912, z=0.2632):    # these initial values are from the statistics of sg government
@@ -186,8 +231,9 @@ def geospatial_vis(geojson_data, type=None):
     
 
 if __name__ == "__main__":
-    vis_geo = gpd.read_file('../data/vis_geo.geojson')
-#     Building = InsideBuilding(total_emission=100000)
-#     Building.cal_user_building()
-#     print(1)
+    gep = gpd.read_file('../data/MasterPlan2019.geojson')
+    classifier = RegionClassifierWithConvexHull('../data/MasterPlan2019.geojson')
+    point = {'longitude':1.307183467, 'latitude':103.7901915}
+    region = classifier.classify(point)
+    print(region)
 
